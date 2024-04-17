fbb26f5da2a1493f974d7c4310c00eb55a5fb62b
edikernel <edison.amg.95@gmail.com>
2024-04-17 10:43:02 +0200

    Add files via upload

---
+++ ItnowForm/src/main/groovy/com/example/ItnowForm/ItnowFormApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.ItnowForm
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ItnowFormApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ItnowFormApplication, args)
+	}
+
+}
---
+++ ItnowForm/src/test/groovy/com/example/ItnowForm/ItnowFormApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.example.ItnowForm
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ItnowFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
